7e35a6de6107632ba7e739c3c16b914f2b9f718a
rhodso <rhodsoprivate@gmail.com>
2021-04-05 18:48:24 +0100

    It's a bit scuffed, but it'll do Story of my life

--- dndCS/src/main/java/com/rhodso/app/sessionBingo.java
+++ dndCS/src/main/java/com/rhodso/app/sessionBingo.java
@@ -6,6 +6,13 @@
 
 package com.rhodso.app;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Insets;
+import java.util.ArrayList;
+import java.util.Collections;
+
 import javax.swing.JButton;
 
 /**
@@ -14,9 +21,66 @@ import javax.swing.JButton;
  */
 public class sessionBingo extends javax.swing.JPanel {
 
+    private javax.swing.JButton buttonArray[];
+    private int buttonState[];
+
     /** Creates new form sessionBingo */
     public sessionBingo() {
+        // Setup button arrays
+        buttonArray = new javax.swing.JButton[26];
+        buttonState = new int[26];
+
+        // Init components
         initComponents();
+
+        // Set states
+        for (int i = 1; i < buttonArray.length; i++) {
+            buttonArray[i].setMaximumSize(new Dimension(100, 100));
+            setButtonState(i);
+        }
+
+        // Get button text
+        String lines[] = { "" };
+
+        // Get list
+        ArrayList<String> tmp = new ArrayList<>();
+        String[] l = bingoLines.getLines();
+
+        // Shuffle and convert
+        for (String s : l) {
+            tmp.add(s);
+        }
+        Collections.shuffle(tmp);
+        lines = new String[tmp.size()];
+        for (int i = 0; i < tmp.size(); i++) {
+            lines[i] = tmp.get(i);
+        }
+
+        // I wanted to do this with a text file but it wasn't
+        // working very well
+
+        // Set text
+        Font f = buttonArray[1].getFont();
+        for (int i = 1; i < buttonArray.length; i++) {
+
+            String buttonText = "<html><center>";
+
+            if (i == 12) {
+                buttonText += "Free Space";
+            } else {
+                buttonText += lines[i];
+            }
+
+            if (lines[i].length() > 25) {
+                buttonArray[i].setFont(new Font(f.getName(), f.getStyle(), 8));
+            } else {
+                buttonArray[i].setFont(new Font(f.getName(), f.getStyle(), 10));
+            }
+
+            buttonText += "</center></html>";
+            buttonArray[i].setText(buttonText);
+        }
+
     }
 
     @SuppressWarnings("unchecked")
@@ -24,113 +88,157 @@ public class sessionBingo extends javax.swing.JPanel {
     // Code">//GEN-BEGIN:initComponents
 
     private void jbutton2ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jbutton2ActionPerformed
-        // TODO add your handling code here:
+        incrementButtonState((JButton) evt.getSource());
     }// GEN-LAST:event_jbutton2ActionPerformed
 
     private void jbutton3ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jbutton3ActionPerformed
-        // TODO add your handling code here:
+        incrementButtonState((JButton) evt.getSource());
     }// GEN-LAST:event_jbutton3ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jButton4ActionPerformed
-        // TODO add your handling code here:
+        incrementButtonState((JButton) evt.getSource());
     }// GEN-LAST:event_jButton4ActionPerformed
 
     private void jbutton1ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jbutton1ActionPerformed
-        // TODO add your handling code here:
+        incrementButtonState((JButton) evt.getSource());
     }// GEN-LAST:event_jbutton1ActionPerformed
 
     private void jButton7ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jButton7ActionPerformed
-        // TODO add your handling code here:
+        incrementButtonState((JButton) evt.getSource());
     }// GEN-LAST:event_jButton7ActionPerformed
 
     private void jButton8ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jButton8ActionPerformed
-        // TODO add your handling code here:
+        incrementButtonState((JButton) evt.getSource());
     }// GEN-LAST:event_jButton8ActionPerformed
 
     private void jButton9ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jButton9ActionPerformed
-        // TODO add your handling code here:
+        incrementButtonState((JButton) evt.getSource());
     }// GEN-LAST:event_jButton9ActionPerformed
 
     private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jButton6ActionPerformed
-        // TODO add your handling code here:
+        incrementButtonState((JButton) evt.getSource());
     }// GEN-LAST:event_jButton6ActionPerformed
 
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jButton5ActionPerformed
-        // TODO add your handling code here:
+        incrementButtonState((JButton) evt.getSource());
     }// GEN-LAST:event_jButton5ActionPerformed
 
     private void jButton10ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jButton10ActionPerformed
-        // TODO add your handling code here:
+        incrementButtonState((JButton) evt.getSource());
     }// GEN-LAST:event_jButton10ActionPerformed
 
     private void jButton11ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jButton11ActionPerformed
-        // TODO add your handling code here:
+        incrementButtonState((JButton) evt.getSource());
     }// GEN-LAST:event_jButton11ActionPerformed
 
     private void jButton12ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jButton12ActionPerformed
-        // TODO add your handling code here:
+        incrementButtonState((JButton) evt.getSource());
     }// GEN-LAST:event_jButton12ActionPerformed
 
     private void jButton13ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jButton13ActionPerformed
-        // TODO add your handling code here:
+        incrementButtonState((JButton) evt.getSource());
     }// GEN-LAST:event_jButton13ActionPerformed
 
     private void jButton14ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jButton14ActionPerformed
-        // TODO add your handling code here:
+        incrementButtonState((JButton) evt.getSource());
     }// GEN-LAST:event_jButton14ActionPerformed
 
     private void jButton15ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jButton15ActionPerformed
-        // TODO add your handling code here:
+        incrementButtonState((JButton) evt.getSource());
     }// GEN-LAST:event_jButton15ActionPerformed
 
     private void jButton16ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jButton16ActionPerformed
-        // TODO add your handling code here:
+        incrementButtonState((JButton) evt.getSource());
     }// GEN-LAST:event_jButton16ActionPerformed
 
     private void jButton17ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jButton17ActionPerformed
-        // TODO add your handling code here:
+        incrementButtonState((JButton) evt.getSource());
     }// GEN-LAST:event_jButton17ActionPerformed
 
     private void jButton18ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jButton18ActionPerformed
-        // TODO add your handling code here:
+        incrementButtonState((JButton) evt.getSource());
     }// GEN-LAST:event_jButton18ActionPerformed
 
     private void jButton19ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jButton19ActionPerformed
-        // TODO add your handling code here:
+        incrementButtonState((JButton) evt.getSource());
     }// GEN-LAST:event_jButton19ActionPerformed
 
     private void jButton20ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jButton20ActionPerformed
-        // TODO add your handling code here:
+        incrementButtonState((JButton) evt.getSource());
     }// GEN-LAST:event_jButton20ActionPerformed
 
     private void jButton21ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jButton21ActionPerformed
-        // TODO add your handling code here:
+        incrementButtonState((JButton) evt.getSource());
     }// GEN-LAST:event_jButton21ActionPerformed
 
     private void jButton22ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jButton22ActionPerformed
-        // TODO add your handling code here:
+        incrementButtonState((JButton) evt.getSource());
     }// GEN-LAST:event_jButton22ActionPerformed
 
     private void jButton23ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jButton23ActionPerformed
-        // TODO add your handling code here:
+        incrementButtonState((JButton) evt.getSource());
     }// GEN-LAST:event_jButton23ActionPerformed
 
     private void jButton24ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jButton24ActionPerformed
-        // TODO add your handling code here:
+        incrementButtonState((JButton) evt.getSource());
     }// GEN-LAST:event_jButton24ActionPerformed
 
     private void jButton25ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jButton25ActionPerformed
-        // TODO add your handling code here:
+        incrementButtonState((JButton) evt.getSource());
     }// GEN-LAST:event_jButton25ActionPerformed
+
+    // Increment the state of the button
+    void incrementButtonState(JButton _sender) {
+
+        // Get index of button in the array
+        int idx = 0;
+        for (JButton b : buttonArray) {
+            if (b == _sender) {
+                break;
+            } else {
+                idx++;
+            }
+        }
+
+        // Increment but not over 4
+        if (buttonState[idx] == 3) {
+            buttonState[idx] = 0;
+        } else {
+            buttonState[idx]++;
+        }
+
+        // Set state
+        setButtonState(idx);
+
+        // JOptionPane.showMessageDialog(mainPanel, "Test " + _sender.getText() + "
+        // index " + idx);
+    }
 
     // Change state of button
     // 0 - Set the button colour scheme to White background, Black text
     // 1 - Set the button colour scheme to Red background, White text
     // 2 - Set the button colour scheme to Orange background, White text
     // 3 - Set the button colour scheme to Greed background, Black text
-    void setButtonState() {
-        // This is going to be cringe because I couldn't create these buttons as part of
-        // a array
+    void setButtonState(int idx) {
+        // Set button colours
+        switch (buttonState[idx]) {
+        case 0:
+            buttonArray[idx].setBackground(Color.WHITE);
+            buttonArray[idx].setForeground(Color.BLACK);
+            break;
+        case 1:
+            buttonArray[idx].setBackground(Color.RED);
+            buttonArray[idx].setForeground(Color.WHITE);
+            break;
+        case 2:
+            buttonArray[idx].setBackground(Color.ORANGE);
+            buttonArray[idx].setForeground(Color.WHITE);
+            break;
+        case 3:
+            buttonArray[idx].setBackground(Color.GREEN);
+            buttonArray[idx].setForeground(Color.BLACK);
+            break;
+        }
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -140,8 +248,6 @@ public class sessionBingo extends javax.swing.JPanel {
     private javax.swing.JScrollPane ScrollPane;
     private javax.swing.JPanel mainPanel;
     // End of variables declaration//GEN-END:variables
-
-    private javax.swing.JButton buttonArray[] = new JButton[26];
 
     private void initComponents() {
 
@@ -176,6 +282,13 @@ public class sessionBingo extends javax.swing.JPanel {
         buttonArray[24] = new javax.swing.JButton();
         buttonArray[25] = new javax.swing.JButton();
 
+        for (int i = 1; i < buttonArray.length; i++) {
+            buttonArray[i].setMaximumSize(new Dimension(100, 100));
+            buttonArray[i].setMinimumSize(new Dimension(100, 100));
+            buttonArray[i].setPreferredSize(new Dimension(100, 100));
+            buttonArray[i].setMargin(new Insets(1, 1, 1, 1));
+        }
+
         HeaderLabel.setFont(new java.awt.Font("Dialog", 0, 48)); // NOI18N
         HeaderLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         HeaderLabel.setText("DnD Session Bingo");
@@ -193,175 +306,150 @@ public class sessionBingo extends javax.swing.JPanel {
                                 .addComponent(HeaderLabel, javax.swing.GroupLayout.DEFAULT_SIZE, 76, Short.MAX_VALUE)
                                 .addContainerGap()));
 
-        buttonArray[1].setText("3");
         buttonArray[1].addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jbutton1ActionPerformed(evt);
             }
         });
 
-        buttonArray[2].setText("0");
         buttonArray[2].addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jbutton2ActionPerformed(evt);
             }
         });
 
-        buttonArray[3].setText("1");
         buttonArray[3].addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jbutton3ActionPerformed(evt);
             }
         });
 
-        buttonArray[4].setText("2");
         buttonArray[4].addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton4ActionPerformed(evt);
             }
         });
 
-        buttonArray[5].setText("4");
         buttonArray[5].addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton5ActionPerformed(evt);
             }
         });
 
-        buttonArray[6].setText("8");
         buttonArray[6].addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton6ActionPerformed(evt);
             }
         });
 
-        buttonArray[7].setText("5");
         buttonArray[7].addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton7ActionPerformed(evt);
             }
         });
 
-        buttonArray[8].setText("6");
         buttonArray[8].addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton8ActionPerformed(evt);
             }
         });
 
-        buttonArray[9].setText("7");
         buttonArray[9].addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton9ActionPerformed(evt);
             }
         });
 
-        buttonArray[10].setText("9");
         buttonArray[10].addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton10ActionPerformed(evt);
             }
         });
 
-        buttonArray[11].setText("14");
         buttonArray[11].addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton11ActionPerformed(evt);
             }
         });
 
-        buttonArray[12].setText("12");
         buttonArray[12].addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton12ActionPerformed(evt);
             }
         });
 
-        buttonArray[13].setText("11");
         buttonArray[13].addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton13ActionPerformed(evt);
             }
         });
 
-        buttonArray[14].setText("10");
         buttonArray[14].addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton14ActionPerformed(evt);
             }
         });
 
-        buttonArray[15].setText("13");
         buttonArray[15].addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton15ActionPerformed(evt);
             }
         });
 
-        buttonArray[16].setText("18");
         buttonArray[16].addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton16ActionPerformed(evt);
             }
         });
 
-        buttonArray[17].setText("17");
         buttonArray[17].addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton17ActionPerformed(evt);
             }
         });
 
-        buttonArray[18].setText("19");
         buttonArray[18].addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton18ActionPerformed(evt);
             }
         });
 
-        buttonArray[19].setText("16");
         buttonArray[19].addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton19ActionPerformed(evt);
             }
         });
 
-        buttonArray[20].setText("15");
         buttonArray[20].addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton20ActionPerformed(evt);
             }
         });
 
-        buttonArray[21].setText("23");
         buttonArray[21].addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton21ActionPerformed(evt);
             }
         });
 
-        buttonArray[22].setText("22");
         buttonArray[22].addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton22ActionPerformed(evt);
             }
         });
 
-        buttonArray[23].setText("24");
         buttonArray[23].addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton23ActionPerformed(evt);
             }
         });
 
-        buttonArray[24].setText("21");
         buttonArray[24].addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton24ActionPerformed(evt);
             }
         });
 
-        buttonArray[25].setText("20");
         buttonArray[25].addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton25ActionPerformed(evt);
